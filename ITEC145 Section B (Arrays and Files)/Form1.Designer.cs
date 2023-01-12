ab7fb981e1ab3054bdf32caeceac2b624eaeaef5
Thomas Gander - School <Thomas.Gander@mymhc.ca>
2023-01-12 09:14:47 -0700

    Finished design

--- ITEC145 Section B (Arrays and Files)/Form1.Designer.cs
+++ ITEC145 Section B (Arrays and Files)/Form1.Designer.cs
@@ -28,12 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtAdd = new System.Windows.Forms.TextBox();
+            this.lstOutput = new System.Windows.Forms.ListBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAverageTemp = new System.Windows.Forms.TextBox();
+            this.btnChallenge = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtJanuary = new System.Windows.Forms.TextBox();
+            this.txtAugust = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 12);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // txtAdd
+            // 
+            this.txtAdd.Location = new System.Drawing.Point(93, 13);
+            this.txtAdd.Name = "txtAdd";
+            this.txtAdd.Size = new System.Drawing.Size(100, 23);
+            this.txtAdd.TabIndex = 1;
+            // 
+            // lstOutput
+            // 
+            this.lstOutput.FormattingEnabled = true;
+            this.lstOutput.ItemHeight = 15;
+            this.lstOutput.Location = new System.Drawing.Point(12, 42);
+            this.lstOutput.Name = "lstOutput";
+            this.lstOutput.Size = new System.Drawing.Size(181, 214);
+            this.lstOutput.TabIndex = 2;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(259, 13);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(117, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Load Weather Data";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(259, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Average Temp =";
+            // 
+            // txtAverageTemp
+            // 
+            this.txtAverageTemp.Location = new System.Drawing.Point(358, 39);
+            this.txtAverageTemp.Name = "txtAverageTemp";
+            this.txtAverageTemp.Size = new System.Drawing.Size(210, 23);
+            this.txtAverageTemp.TabIndex = 5;
+            // 
+            // btnChallenge
+            // 
+            this.btnChallenge.Location = new System.Drawing.Point(259, 81);
+            this.btnChallenge.Name = "btnChallenge";
+            this.btnChallenge.Size = new System.Drawing.Size(75, 23);
+            this.btnChallenge.TabIndex = 6;
+            this.btnChallenge.Text = "Challenge";
+            this.btnChallenge.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Average Temp in Jan =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(259, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Average Temp in Aug =";
+            // 
+            // txtJanuary
+            // 
+            this.txtJanuary.Location = new System.Drawing.Point(391, 116);
+            this.txtJanuary.Name = "txtJanuary";
+            this.txtJanuary.Size = new System.Drawing.Size(177, 23);
+            this.txtJanuary.TabIndex = 9;
+            // 
+            // txtAugust
+            // 
+            this.txtAugust.Location = new System.Drawing.Point(391, 150);
+            this.txtAugust.Name = "txtAugust";
+            this.txtAugust.Size = new System.Drawing.Size(177, 23);
+            this.txtAugust.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAugust);
+            this.Controls.Add(this.txtJanuary);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnChallenge);
+            this.Controls.Add(this.txtAverageTemp);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.lstOutput);
+            this.Controls.Add(this.txtAdd);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnAdd;
+        private TextBox txtAdd;
+        private ListBox lstOutput;
+        private Button btnLoad;
+        private Label label1;
+        private TextBox txtAverageTemp;
+        private Button btnChallenge;
+        private Label label2;
+        private Label label3;
+        private TextBox txtJanuary;
+        private TextBox txtAugust;
     }
 }
