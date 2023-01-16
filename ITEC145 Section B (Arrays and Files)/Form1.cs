2a4784ac956ca40e88033c10d5ccacab1f616e98
Thomas Gander - School <Thomas.Gander@mymhc.ca>
2023-01-16 10:32:50 -0700

    Step 1 Complete

--- ITEC145 Section B (Arrays and Files)/Form1.cs
+++ ITEC145 Section B (Arrays and Files)/Form1.cs
@@ -2,9 +2,56 @@ namespace ITEC145_Section_B__Arrays_and_Files_
 {
     public partial class Form1 : Form
     {
+        List<string> names = new List<string>();
+        
+        
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string nameInput = txtAdd.Text;                                          //Assigns text box to nameInput variable
+
+                if(nameInput.Contains(""))                                              //Checks for blank fields
+                {
+                    MessageBox.Show("Only enter strings and no spaces");                //Message for blanks strings
+                }
+                else
+                {
+                    lstOutput.Items.Clear();                                            //Clears list
+
+                    StreamWriter outputFile = new StreamWriter("NamesLog.txt", true);   //StreamWriter for saving files
+
+                    outputFile.WriteLine(nameInput);                                    //Write a line to output file NamesLog.txt
+
+                    outputFile.Close();                                                 //Closes the outputFile
+
+                    names.Clear();                                                      //Clears the names list
+
+                    StreamReader inputFile = new StreamReader("NamesLog.txt");          //Creates a streamreader to read the outputfile
+
+                    while(inputFile.EndOfStream != true)                                //runs loop until the reader hits the end of stream
+                    {
+                        names.Add(inputFile.ReadLine());                                //Adds the stream reader lines to the names list
+                    }
+                    inputFile.Close();                                                  //Closes the streamreader process
+
+                    foreach(string name in names)                                       //Checks all the strings in names list
+                    {
+                        lstOutput.Items.Add(name);                                      //Adds names list strings to the listbox
+                    }
+
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);                                            //In case all else fails with my program
+            }
         }
     }
 }
